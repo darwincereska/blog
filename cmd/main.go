e1f0a638b9386e6ca2eb521234696cf860821ee2
darwincereska <accfordarwin@gmail.com>
2026-02-18 06:25:35 -0500

    feat: testing

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"blog/internal/database"
-	"blog/internal/config"
-	"github.com/charmbracelet/log"
 	"blog/internal/cache"
+	"blog/internal/config"
+	"blog/internal/database"
 	"blog/internal/services"
+	"context"
+	"os"
+
+	"github.com/charmbracelet/log"
 )
 
 func main() {
@@ -26,8 +29,25 @@ func main() {
 
 	// Create Redis caches
 	strapi_cache := cache.CreateCache(server_config.RedisHost, server_config.RedisPort, 0)
-	analytics_cache := cache.CreateCache(server_config.RedisHost, server_config.RedisPort, 1)
+	// analytics_cache := cache.CreateCache(server_config.RedisHost, server_config.RedisPort, 1)
 
 	// Create Strapi service
-	strapi_service := services.NewStrapiService(server_config.StrapiHost, server_config.StrapiApiKey, strapi_cache)
+	strapi_service := services.NewStrapiService(server_config.StrapiEndpoint+"/graphql", server_config.StrapiToken, strapi_cache)
+
+	// Strapi logger
+	strapi_logger := log.NewWithOptions(os.Stderr, log.Options{
+		ReportTimestamp: true,
+		Prefix: "STRAPI",
+	})
+
+	// Test strapi get
+	posts, err := strapi_service.GetFeaturedPosts(context.Background(), 10, 1) 
+	if err != nil {
+		strapi_logger.Error(err)
+		os.Exit(0)
+	}
+	
+	post := posts[0]
+
+	strapi_logger.Info(post)
 }
--- internal/config/server.go
+++ internal/config/server.go
@@ -8,10 +8,10 @@ import (
 type ServerConfig struct {
 	Host string
 	Port int
-	StrapiHost string
+	StrapiEndpoint string
 	RedisHost string
 	RedisPort int
-	StrapiApiKey string
+	StrapiToken string
 	CacheTTL int
 	EchoMode string
 }
@@ -23,8 +23,8 @@ func NewServerConfig() *ServerConfig {
 func (c *ServerConfig) LoadConfig() {
 	c.Host = env.GetString("HOST", "0.0.0.0")
 	c.Port = env.GetInt("PORT", 3000)
-	c.StrapiHost = env.GetString("STRAPI_HOST", "https://strapi.darwincereska.dev")
-	c.StrapiApiKey = env.GetString("STRAPI_API_KEY", "")
+	c.StrapiEndpoint = env.GetString("STRAPI_ENDPOINT", "https://strapi.darwincereska.dev")
+	c.StrapiToken = env.GetString("STRAPI_TOKEN", "")
 	c.RedisHost = env.GetString("REDIS_HOST", "localhost")
 	c.RedisPort = env.GetInt("REDIS_PORT", 6379)
 	c.CacheTTL = env.GetInt("CACHE_TTL", 3600)
@@ -34,7 +34,7 @@ func (c *ServerConfig) LoadConfig() {
 	log.Info("Host", "host", c.Host)
 	log.Info("Port", "port", c.Port)
 	log.Info("Redis Host", "host", c.RedisHost)
-	log.Info("Strapi URL", "host", c.StrapiHost)
+	log.Info("Strapi URL", "host", c.StrapiEndpoint)
 	log.Info("Echo Mode", "mode", c.EchoMode)
 	log.Info("Cache TTL", "ttl", c.CacheTTL)
 }
